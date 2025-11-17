f2fef761d89cd1dcb1e10b62ba911d32da205bbc
emman-j <176351546+emman-j@users.noreply.github.com>
2025-11-17 09:34:14 +0800

    test implementation of tutorial manager

--- practice-projects/test-app-tutorial/test-app-tutorial/Form1.cs
+++ practice-projects/test-app-tutorial/test-app-tutorial/Form1.cs
@@ -1,20 +1,24 @@
+using System.Threading;
+
 namespace test_app_tutorial
 {
     public partial class Form1 : Form
     {
+        TutorialManager tutorial;
         public Form1()
         {
             InitializeComponent();
 
+            tutorial = new TutorialManager(new List<(Control, string)>
+            {
+                (button1, "Click here to connect to the device"),
+                (textBox1, "Enter the device ID here")
+            });
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            var overlay = new TutorialOverlay(button1, "Click this button to start");
-            overlay.Show();
-
-            overlay = new TutorialOverlay(textBox1, "Click this button to start");
-            overlay.Show();
+            tutorial.Start();
         }
     }
 }
