103e1b1d1814cfc68d5300e78e812cde9086dbdf
emman-j <176351546+emman-j@users.noreply.github.com>
2026-01-26 17:46:54 +0800

    added CRC16Modbus with lookuptable implementation

---
+++ practice-projects/modbus_crc_lookuptable/modbus_crc_lookuptable/CRC16Modbus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modbus_crc_lookuptable
+{
+    public class CRC16Modbus
+    {
+        // Modbus CRC-16 polynomial (reflected)
+        private static readonly ushort POLYNOMIAL = 0xA001;
+
+        // Precomputed CRC-16 lookup table
+        private static readonly ushort[] CRC16Table = new ushort[256];
+
+        static CRC16Modbus()
+        {
+            // Initialize the lookup table
+            for (ushort i = 0; i < 256; i++)
+            {
+                ushort crc = i;
+                for (ushort j = 8; j > 0; j--)
+                {
+                    if ((crc & 0x0001) != 0)
+                    {
+                        crc >>= 1;
+                        crc ^= POLYNOMIAL;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+                CRC16Table[i] = crc;
+            }
+        }
+
+        public static ushort CalculateCRC(byte[] data)
+        {
+            ushort crc = 0xFFFF;  // Initial value
+            foreach (byte b in data)
+            {
+                byte tableIndex = (byte)(((crc) & 0xFF) ^ b);  // Get the lower byte of the current CRC and XOR with current byte
+                crc = (ushort)((crc >> 8) ^ CRC16Table[tableIndex]);  // Update CRC using the lookup table
+            }
+            return crc;
+        }
+    }
+}
